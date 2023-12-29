dfe2121b827a7cb74d7b2c10114f9307b71d074b
Liubov2909 <139795007+Liubov2909@users.noreply.github.com>
2023-12-29 15:42:56 +0000

    Deploying to gh-pages from @ Liubov2909/goit-js-hw-11@5c4cfd17f671aab6575a6aa54cb47855a6671f8d 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,10 @@
+import{i as l}from"./assets/vendor-59215b7c.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&n(i)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerpolicy&&(r.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?r.credentials="include":e.crossorigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function n(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();const a=new URLSearchParams({key:"41529899-34462864becb6db8698a10d1b",q:"black",image_type:"photo",orientation:"horizontal",safesearch:!0});fetch(`https://pixabay.com/api/?${a}`).then(t=>{if(!t.ok)throw new Error("Sorry, there are no images matching your search query. Please try again!");return t.json()}).then(t=>{console.log(t);const o=document.querySelector(".gallery"),s=t.reduce((n,e)=>n+`<li class="gallery-item">
+        <a href=${e.largeImageURL}> 
+          <img src =${e.webformatURL} alt=${e.tags}/>
+        </a>
+      <p>Likes: ${e.likes}</p>
+      <p>views: ${e.views}</p>
+      <p>comments: ${e.comments}</p>
+      <p>downloads: ${e.downloads}</p>
+      </li>`,"");o.innerHTML=s}).catch(t=>{l.error({position:"bottomCenter",message:t.message})});
+//# sourceMappingURL=commonHelpers.js.map
